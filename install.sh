fcb55f390bef9d4009dc5ffebb14b3b9581dff2e
Josh <josh@hitsystems.com.au>
2020-05-14 15:49:36 +1000

    Add sudo request to beginning of script

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 
 echo "Setting up your Mac..."
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `install.sh` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
